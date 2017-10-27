6ce72c93689fb38877e71ac9de80c49f12edfda6
S2 <s@ireoo.com>
2017-10-27 22:20:04 +0800

    update

--- test/main.js
+++ test/main.js
@@ -60,8 +60,10 @@ if (fs.existsSync(path.join(__dirname, `code.json`))) {
 
 // for (let c = 0; c < 100; c++) {
 setInterval(function () {
-    Q.go(build);
-    // console.log(JSON.stringify({ code: c, str: str }));
+    getCode(function (str, c) {
+        Q.go(build, [str, c]);
+        // console.log(JSON.stringify({ code: c, str: str }));
+    });
 }, 1);
 
 // setInterval(function () {
@@ -78,7 +80,7 @@ setInterval(function () {
 // }, 1);
 
 
-function run(file, code, cb = () => { }) {
+function run(file, code, cb = function () { }) {
     let c = path.join(__dirname, `code/${file}.c`);
     let exe = path.join(__dirname, `exe/${file}`);
     let run = spawn(exe);
@@ -88,33 +90,32 @@ function run(file, code, cb = () => { }) {
     run.stdout.on('data', (data) => {
         debug.wran(`${JSON.stringify(code)} say: ${data.toString()}`);
         str += data.toString();
-        email(`[${code.length}] say something in AI`, code, data.toString(), function () {
+        email(`${JSON.stringify(code)} say something in AI`, code.str, data.toString(), function () {
             console.log('[${file}] Send email is OK!');
         });
     });
 
     run.stderr.on('data', (data) => {
-        console.error(`[${code.length}] error: ${data.toString()}`);
+        console.error(`${JSON.stringify(code)} error: ${data.toString()}`);
         err += data.toString();
-        email(`[${code.length}] say something with error in AI`, code.str, data.toString(), function () {
+        email(`${JSON.stringify(code)} say something with error in AI`, code.str, data.toString(), function () {
             console.log('[${file}] Send email is OK!');
         });
     });
 
     run.on('close', (data) => {
-        console.log(`[${code.length}] child process exited with code ${data}`);
+        console.log(`${JSON.stringify(code)} child process exited with code ${data}`);
         if (err !== '' || str === '') {
             if (fs.existsSync(c)) fs.unlinkSync(c);
             if (fs.existsSync(exe)) fs.unlinkSync(exe);
         } else if (str !== '') {
-            debug.wran(`[${code.length}] exit; All say: ${data.toString()}`);
+            debug.wran(`${JSON.stringify(code)} exit; All say: ${data.toString()}`);
         }
         cb(data);
     });
 }
 
-function build() {
-    let str = getCode();
+function build(str, c) {
     try {
         let file = crypto.createHash('md5').update(str).digest('hex').toUpperCase(); //32位大写 
 
@@ -127,26 +128,26 @@ int main()
 {
     ${str}
     return 0;
-}`, (err) => {
+}`, function (err) {
                 if (!err) {
                     if (!fs.existsSync(require('path').join(__dirname, "exe"))) {
                         fs.mkdir(require('path').join(__dirname, "exe"));
                     }
-                    exec(`gcc ${path.join(__dirname, `code/${file}.c`)} -o ${path.join(__dirname, `exe/${file}`)}`, (err, stdout, stderr) => {
+                    exec(`gcc ${path.join(__dirname, `code/${file}.c`)} -o ${path.join(__dirname, `exe/${file}`)}`, function (err, stdout, stderr) {
                         if (!err) {
                             if (fs.existsSync(path.join(__dirname, `exe/${file}`))) {
                                 // console.log(`[${str.length}] ${file} Compile successfully!`);
-                                run(file, str, () => { });
+                                run(file, { str: str, code: c });
                             } else {
                                 // console.log(`[${str.length}] ${file} Compile failure!`);
                                 if (fs.existsSync(require('path').join(__dirname, `code/${file}.c`))) fs.unlinkSync(path.join(__dirname, `code/${file}.c`));
                             }
                         } else {
-                            console.error(stderr);
-                            console.error(`[${str.length}] ${file} Compile error!`);
+                            // console.error(stderr);
+                            // console.error(`[${str.length}] ${file} Compile error! Error code: ${JSON.stringify({ code: c, str: str })}`);
                             if (fs.existsSync(require('path').join(__dirname, `code/${file}.c`))) fs.unlinkSync(path.join(__dirname, `code/${file}.c`));
                         }
-                        // fs.writeFileSync(path.join(__dirname, `code.json`), JSON.stringify({ code: c, str: str }));
+                        fs.writeFileSync(path.join(__dirname, `code.json`), JSON.stringify({ code: c, str: str }));
                     });
                 }
             });
@@ -155,17 +156,43 @@ int main()
     }
 }
 
-function getCode() {
-    let s = '', str = '', i = 0;
+function getCode(cb) {
+    if (!code) code = {
+        code: [0],
+        str: '1'
+    };
 
-    while (s !== ';' || (s === ';' && i !== 1)) {
-
-        s = String.fromCharCode(Math.floor(Math.random() * 128));
-        if (s === ';') i = Math.floor(Math.random() * 2);
-        str += s;
-        // console.log(str);
-        if (/^#/.test(str)) str = '';
+    code.code[code.code.length - 1]++;
+    for (let a = code.code.length - 1; a >= 0; a--) {
+        if (code.code[a] >= initString.length) {
+            if (a !== 0) {
+                code.code[a] = 0;
+                code.code[a - 1]++;
+            } else {
+                for (let b = 0; b < code.code.length; b++) {
+                    code.code[b] = 0;
+                }
+                code.code.push(0);
+            }
+        }
     }
 
-    return str;
+    let c = [];
+    for (let w = 0; w < code.code.length; w++) {
+        c.push(code.code[w]);
+    }
+
+    let str = '';
+    for (let w = 0; w < code.code.length; w++) {
+        str += initString[code.code[w]];
+    }
+
+    code.str = str;
+
+    cb(str, c);
 }
+
+// process.on('exit', function () {
+//     fs.writeFileSync(path.join(__dirname, `code.json`), JSON.stringify(code));
+//     console.log('exit');
+// });
